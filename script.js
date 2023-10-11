107455edd22e57fd6aa119726454ac98998ee358
kevin <kevindubon43@gmail.com>
2023-10-11 11:00:05 -0700

    Added dark theme with button.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const themeBtn = document.querySelector('#theme');
+const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+const subSection = document.querySelector('.subsection');
+const projectLinks = document.querySelector('.squares');
+const quotes = document.querySelector('.quote');
+const action = document.querySelector('.action');
+const footer = document.querySelector('.footer');
 let timeDisplay = document.getElementById('time');
 
 function refreshTime() {
@@ -7,3 +15,43 @@ function refreshTime() {
 }
 
 setInterval(refreshTime, 1000);
+
+themeBtn.addEventListener('click', () => {
+    themeBtn.textContent = 'Light';
+    header.classList.toggle('darker');
+    main.classList.toggle('darkTheme');
+    subSection.classList.toggle('darker');
+    projectLinks.classList.toggle('darker');
+    action.classList.toggle('darkTheme');
+    quotes.classList.toggle('darker');
+    footer.classList.toggle('darker');
+    subSection.style.borderBottom = 'none';
+    footer.style.borderBottom = 'none';
+    if(subSection.classList.contains('darker')) {
+        themeBtn.textContent = 'Light';
+    }
+    else {
+        themeBtn.textContent = 'Dark';
+    }
+});
+
+function enableDark() {
+    themeBtn.textContent = 'Light';
+    header.classList.toggle('darker');
+    main.classList.toggle('darkTheme');
+    subSection.classList.toggle('darker');
+    projectLinks.classList.toggle('darker');
+    action.classList.toggle('darkTheme');
+    quotes.classList.toggle('darker');
+    footer.classList.toggle('darker');
+    subSection.style.borderBottom = 'none';
+    footer.style.borderBottom = 'none';
+    if(subSection.classList.contains('darker')) {
+        themeBtn.textContent = 'Light';
+    }
+    else {
+        themeBtn.textContent = 'Dark';
+    }
+}
+
+enableDark();
